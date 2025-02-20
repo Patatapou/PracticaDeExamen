d56bae15ed640fcaf86508e8ecdea029c77b0332
Patatapou <migel2013@outlook.com>
2025-02-19 19:07:33 -0600

    Agregar archivos de proyecto.

---
+++ PracticaDeExamen/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PracticaDeExamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            NOM = new TextBox();
+            CURP = new TextBox();
+            label2 = new Label();
+            BUSCADOR = new TextBox();
+            label3 = new Label();
+            dgv = new DataGridView();
+            button1 = new Button();
+            NOMBRE = new DataGridViewTextBoxColumn();
+            EDAD = new DataGridViewTextBoxColumn();
+            ESTADO = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgv).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre";
+            // 
+            // NOM
+            // 
+            NOM.Location = new Point(12, 32);
+            NOM.Name = "NOM";
+            NOM.Size = new Size(272, 27);
+            NOM.TabIndex = 1;
+            // 
+            // CURP
+            // 
+            CURP.Location = new Point(335, 32);
+            CURP.Name = "CURP";
+            CURP.Size = new Size(272, 27);
+            CURP.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(335, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 20);
+            label2.TabIndex = 2;
+            label2.Text = "CURP";
+            // 
+            // BUSCADOR
+            // 
+            BUSCADOR.Location = new Point(12, 112);
+            BUSCADOR.Name = "BUSCADOR";
+            BUSCADOR.Size = new Size(272, 27);
+            BUSCADOR.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 20);
+            label3.TabIndex = 4;
+            label3.Text = "BUSCAR";
+            // 
+            // dgv
+            // 
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv.Columns.AddRange(new DataGridViewColumn[] { NOMBRE, EDAD, ESTADO });
+            dgv.Location = new Point(3, 145);
+            dgv.Name = "dgv";
+            dgv.RowHeadersWidth = 51;
+            dgv.Size = new Size(795, 293);
+            dgv.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(650, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 7;
+            button1.Text = "REGISTRAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // NOMBRE
+            // 
+            NOMBRE.Frozen = true;
+            NOMBRE.HeaderText = "NOMBRE";
+            NOMBRE.MinimumWidth = 6;
+            NOMBRE.Name = "NOMBRE";
+            NOMBRE.ReadOnly = true;
+            NOMBRE.Width = 300;
+            // 
+            // EDAD
+            // 
+            EDAD.Frozen = true;
+            EDAD.HeaderText = "EDAD";
+            EDAD.MinimumWidth = 6;
+            EDAD.Name = "EDAD";
+            EDAD.ReadOnly = true;
+            EDAD.Width = 250;
+            // 
+            // ESTADO
+            // 
+            ESTADO.Frozen = true;
+            ESTADO.HeaderText = "ESTADO";
+            ESTADO.MinimumWidth = 6;
+            ESTADO.Name = "ESTADO";
+            ESTADO.ReadOnly = true;
+            ESTADO.Width = 125;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(dgv);
+            Controls.Add(BUSCADOR);
+            Controls.Add(label3);
+            Controls.Add(CURP);
+            Controls.Add(label2);
+            Controls.Add(NOM);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dgv).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox NOM;
+        private TextBox CURP;
+        private Label label2;
+        private TextBox BUSCADOR;
+        private Label label3;
+        private DataGridView dgv;
+        private Button button1;
+        private DataGridViewTextBoxColumn NOMBRE;
+        private DataGridViewTextBoxColumn EDAD;
+        private DataGridViewTextBoxColumn ESTADO;
+    }
+}
---
+++ PracticaDeExamen/Form1.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace PracticaDeExamen
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        ArrayList registros = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombre = NOM.Text;
+            string curp = CURP.Text;
+            dgv.Rows.Add();
+            dgv.Rows[i].Cells[0].Value = nombre;
+            dgv.Rows[i].Cells[1].Value = edadYestado(curp)[1];
+            dgv.Rows[i].Cells[2].Value = edadYestado(curp)[0];
+
+            string[] registro = { dgv.Rows[i].Cells[0].ToString(), dgv.Rows[i].Cells[1].ToString(), dgv.Rows[i].Cells[2].ToString() };
+            registros.Add(registro[0]);
+            registros.Add(registro[1]);
+            registros.Add(registro[2]);
+
+            NOM.Clear();
+            CURP.Clear();
+            i++;
+        }
+        public string[] edadYestado(string curp)
+        {
+            string[] res = new string[2];
+            string expresion = "^[A-Z]{4}\\d{6}[HM]{1}[A-Z]{2}[A-Z]{3}[0-9A-Z]{2}$";
+            if (Regex.IsMatch(curp, expresion) == true)
+            {
+                //ESTADO y fecha de nacimiento
+                string year = curp[4].ToString() + curp[5].ToString();
+                string mes = curp[6].ToString() + curp[7].ToString();
+                string dia = curp[8].ToString() + curp[9].ToString();
+
+                string estado = curp[11].ToString() + curp[12].ToString();
+                DateTime fechaNac = DateTime.Parse(dia + "/" + mes + "/" + year);
+                switch (estado)
+                {
+                    case "AG": estado = "Aguascalientes"; break;
+                    case "BC": estado = "Baja California"; break;
+                    case "BS": estado = "Baja California Sur"; break;
+                    case "CC": estado = "Campeche"; break;
+                    case "CS": estado = "Chiapas"; break;
+                    case "CH": estado = "Chihuahua"; break;
+                    case "CM": estado = "Coahuila"; break;
+                    case "DG": estado = "Durango"; break;
+                    case "GT": estado = "Guanajuato"; break;
+                    case "GR": estado = "Guerrero"; break;
+                    case "HG": estado = "Hidalgo"; break;
+                    case "JC": estado = "Jalisco"; break;
+                    case "MC": estado = "México"; break;
+                    case "MN": estado = "Michoacán"; break;
+                    case "MS": estado = "Morelos"; break;
+                    case "NT": estado = "Nayarit"; break;
+                    case "NL": estado = "Nuevo León"; break;
+                    case "OC": estado = "Oaxaca"; break;
+                    case "PL": estado = "Puebla"; break;
+                    case "QT": estado = "Querétaro"; break;
+                    case "QR": estado = "Quintana Roo"; break;
+                    case "SP": estado = "San Luis Potosí"; break;
+                    case "SL": estado = "Sinaloa"; break;
+                    case "SR": estado = "Sonora"; break;
+                    case "TC": estado = "Tabasco"; break;
+                    case "TS": estado = "Tamaulipas"; break;
+                    case "TL": estado = "Tlaxcala"; break;
+                    case "VZ": estado = "Veracruz"; break;
+                    case "YN": estado = "Yucatán"; break;
+                    case "ZS": estado = "Zacatecas"; break;
+                    default: estado = "Abreviatura no reconocida"; break;
+                }
+                res[0] = estado;
+
+                //EDAD
+                DateTime hoy = DateTime.Today;
+                TimeSpan edadHoras = hoy.Date - fechaNac.Date;
+                DateTime edad = new DateTime() + edadHoras;
+
+                if (hoy.Month < fechaNac.Month)
+                {
+                    res[1]=((edad.Year - 1) + " años - " + (edad.Month - 1) + " meses - " + edad.Day + " dias");
+                }
+                else
+                {
+                    res[1] = (edad.Year + " años - " + edad.Month + " meses - " + edad.Day + " dias");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese una CURP valida");
+            }
+            return res;
+        }
+    }
+}
